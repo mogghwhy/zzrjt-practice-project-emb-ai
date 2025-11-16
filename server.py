38c6231d589c707c22ba040331fd3e3428f78e2c
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2025-11-16 12:51:10 -0500

    render html template and run the app on the port

--- server.py
+++ server.py
@@ -32,7 +32,9 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000)
